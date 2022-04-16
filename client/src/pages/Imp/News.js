2bae09dcd205c473e7e80f733eef3e77c43f355b
unknown <prabath096@gmail.com>
2022-04-15 23:26:43 -0700

    f13

--- client/src/pages/Imp/News.js
+++ client/src/pages/Imp/News.js
@@ -22,7 +22,7 @@ const [news, setNews] = React.useState();
 <div>
 <NavigationBar/>
 <div className='p-4 container'>
-{/* {process.env.REACT_APP_UNSPLASH_KEY} */}
+
 
 <div className='text-secondary fs-1 mt-5 mb-2'>Top Healthcare News Headlines</div>
 {!news ? <div className='text-center fs-3 mt-5 pt-5 text-secondary' ><div class="spinner-border" role="status">
@@ -33,15 +33,15 @@ const [news, setNews] = React.useState();
     <div className="card mb-4" >
     <div className="row g-0">
         <div className="col-md-4">
-        <img src={data.urlToImage} className="img-fluid rounded-start" alt={data.urlToImage} 
+        <img src={data.image} className="img-fluid rounded-start" alt={data.image} 
         style={{height: '100%', backgroundSize: 'cover' }}/>
         </div>
         <div className="col-md-8 bg-light ">
         <div className="card-body p-4">
             <h3 className="card-title mb-4">{data.title}</h3> 
-            <p className="card-text">{data.description}</p>
+            <p className="card-text">{data.content}</p>
             <p className="card-text"><small className="text-muted"> Published {data.publishedAt} </small></p>
-            <p className="card-text"><small className="text-muted"> {data.author} </small></p>
+            <p className="card-text"><small className="text-muted"> {data.source.name} </small></p>
             <a href={data.url} className='text-decoration-none' target="_blank" rel="noopener"> Source <i class="bi bi-box-arrow-up-right"></i></a>
         </div>
         </div>
